ffcf865765801b0e2acfda1fbb1ee058e0cf716d
Moamen Sherif <102827938+MoamenSherif81@users.noreply.github.com>
2022-12-10 00:09:52 +0200

    Add files via upload

--- JS/app.js
+++ JS/app.js
@@ -1,60 +1,97 @@
-let btn = document.getElementById('add-task');
-let input = document.getElementById('task-title');
-let taskCont = document.querySelector('.tasks');
+const btn = document.getElementById('add-task');
+const input = document.getElementById('task-title');
+const taskCont = document.querySelector('.tasks');
+const filterCont = document.querySelector('.filter');
+const filterInp = document.querySelector('.filter-input');
+let TasksArr;
+if(window.localStorage['number'] == undefined) window.localStorage['number'] = 0;
+TasksArr = JSON.parse(localStorage.getItem("TasksArr") || "[]");
+input.focus();
+
+window.addEventListener('load', () =>{
+  if(TasksArr.length != 0){
+    taskCont.children[0].classList.toggle('hide');
+    taskCont.children[1].classList.toggle('hide');
+    TasksArr.forEach((element) => {
+      const newTask = document.createElement('li');
+      newTask.classList.add('task');
+      newTask.setAttribute('taskId', element.id);
+      newTask.innerHTML = `
+        <div class="divider"></div>
+        <div class="task-content">
+          <div class="inputs">
+            <div class="task-title">${element.title}</div>
+            <input type="text" class="edit-task hide" onkeypress="editEnter(event, this)">
+            <div class="task-date">${element.date}</div>
+          </div>
+          <div class=buttons>
+            <button class="btn-floating edit-btn" onclick='editEle(this)'><i class="material-icons">edit</i></button>
+            <button class="btn-floating remove-btn" onclick='removeEle(this)'><i class="material-icons">close</i></button>
+          </div>
+        </div>`;
+      taskCont.append(newTask);
+    });
+  }
+})
 
 btn.addEventListener('click', () => {
-  if(input.value == '' || input.value.trim().length === 0){
+  if(input.value.trim().length === 0){
     Swal.fire({icon: 'error', title: 'Error', text:'Enter the task name!'});
   } else {
-    let date = new Date();
-    let newTask = document.createElement('li');
-    newTask.classList.add('task', 'show');
+    const date = new Date();
+    const newTask = document.createElement('li');
+    newTask.classList.add('task');
+    newTask.setAttribute('taskId', window.localStorage.number++);
     newTask.innerHTML = `
-      <div class="inputs">
-        <div class="task-title">${input.value}</div>
-        <input type="text" class="edit-task hide" onkeypress="editEnter(event, this)">
-        <div class="task-date">${date.toLocaleDateString() + ' ' + date.toLocaleTimeString()}</div>
-      </div>
-      <div class=buttons>
-        <button class="btn-floating edit-btn" onclick='editEle(this)'><i class="material-icons">edit</i></button>
-        <button class="btn-floating remove-btn" onclick='removeEle(this)'><i class="material-icons">close</i></button>
+      <div class="divider"></div>
+      <div class="task-content show">
+        <div class="inputs">
+          <div class="task-title">${input.value}</div>
+          <input type="text" class="edit-task hide" onkeypress="editEnter(event, this)">
+          <div class="task-date">${date.toLocaleDateString() + ' ' + date.toLocaleTimeString()}</div>
+        </div>
+        <div class=buttons>
+          <button class="btn-floating edit-btn" onclick='editEle(this)'><i class="material-icons">edit</i></button>
+          <button class="btn-floating remove-btn" onclick='removeEle(this)'><i class="material-icons">close</i></button>
+        </div>
       </div>`;
-    if(taskCont.children[0].children[0].className == 'no-tasks') {
-      taskCont.innerHTML = '';
-      taskCont.append(newTask);
+    if(taskCont.children.length == 2) {
+      taskCont.children[0].classList.toggle('hide');
+      taskCont.children[1].classList.toggle('hide');
     }else{
-      taskCont.lastElementChild.classList.remove('show');
-      let divider = document.createElement('div');
-      divider.classList.add('divider'); 
-      taskCont.append(divider);
-      taskCont.append(newTask);
+      taskCont.lastElementChild.children[1].classList.remove('show');
     }
+    taskCont.append(newTask);
+    TasksArr.push({id: (window.localStorage.number)-1, title: input.value, date: date.toLocaleDateString() + ' ' + date.toLocaleTimeString()});
+    window.localStorage['TasksArr'] = JSON.stringify(TasksArr);
+    console.log(TasksArr);
     input.value = '';
+    input.focus(); 
   }
 });
 
 function removeEle(ele){
-  let parent = ele.parentNode.parentNode;
-  parent.classList.add('remove');
-  parent.addEventListener('animationend', () => {
-    if(parent.nextElementSibling != null){
-      parent.nextElementSibling.remove();
-    }else if(parent.previousElementSibling != null){
-      parent.previousElementSibling.remove();
-    }
+  const parent = ele.parentNode.parentNode.parentNode;
+  parent.lastElementChild.classList.add('remove');
+  parent.lastElementChild.addEventListener('animationend', () => {
+    const taskId = TasksArr.findIndex(item => item.id == parent.getAttribute('taskId'));
+    TasksArr.splice(taskId, 1);
+    window.localStorage.setItem('TasksArr', JSON.stringify(TasksArr));
     parent.remove();
-    if(taskCont.children[0] == null){
-      taskCont.innerHTML = `<li><h1 class="no-tasks">No Tasks Yet</h1></li>`;
+    if(taskCont.children.length == 2){
+      taskCont.children[0].classList.toggle('hide');
+      taskCont.children[1].classList.toggle('hide');
     }
   })
+  console.log(TasksArr);
 }
 
 function editEle(ele){
-  let parent = ele.parentNode.parentNode;
-  let taskTitle = parent.children[0].children[0];
-  let taskEditInput = parent.children[0].children[1];
+  const parent = ele.parentNode.parentNode;
+  const taskTitle = parent.children[0].children[0];
+  const taskEditInput = parent.children[0].children[1];
   if(taskEditInput.classList.contains('hide')){
-    let tasks = document.querySelectorAll('.task');
+    const tasks = document.querySelectorAll('.task');
     for(let i = 0; i < tasks.length; i++){
       if(!tasks[i].querySelector('.edit-task').classList.contains('hide')){
         tasks[i].querySelector('.edit-task').classList.toggle('hide');
@@ -66,7 +103,7 @@ function editEle(ele){
     taskEditInput.value = taskTitle.innerHTML;
     ele.children[0].innerText = 'done';
   } else {
-    if(taskEditInput.value == '' || taskEditInput.value.trim().length == 0){
+    if(taskEditInput.value.trim().length == 0){
       Swal.fire({icon: 'error', title: 'Error', text:'Enter a valid task name!'});
     } else {
       taskTitle.innerHTML = taskEditInput.value;
@@ -77,6 +114,18 @@ function editEle(ele){
   taskTitle.classList.toggle('hide');
   taskEditInput.classList.toggle('hide');
 }
+
+filterInp.addEventListener('keyup', () =>{
+  const tasks = [...taskCont.children];
+  for(let i = 2; i < tasks.length; i++){
+    if(!tasks[i].children[1].firstElementChild.innerText.toLowerCase().includes(filterInp.value.toLowerCase())){
+      tasks[i].classList.add('hide');
+    } else{
+      tasks[i].children[1].classList.remove('show');
+      tasks[i].classList.remove('hide');
+    }
+  }
+})
 
 input.addEventListener('keypress', (event) => {
   if(event.key == 'Enter'){
